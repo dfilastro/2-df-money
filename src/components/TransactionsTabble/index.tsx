2a2ff42dd7a00fd261bf9b6cfb05ea68fbe78275
dfilastro <d.filastro@hotmail.com>
2022-02-19 08:47:26 -0300

    created modal structure

--- src/components/TransactionsTabble/index.tsx
+++ src/components/TransactionsTabble/index.tsx
@@ -1,6 +1,12 @@
+import { useEffect } from 'react';
+import { api } from '../../services/api';
 import { Container } from './styles';
 
 export function TransactionsTable() {
+  useEffect(() => {
+    api.get('transactions').then((response) => console.log(response.data));
+  }, []);
+
   return (
     <Container>
       <table>
@@ -16,21 +22,21 @@ export function TransactionsTable() {
         <tbody>
           <tr>
             <td>Desenvolvimento de website</td>
-            <td>R$ 12.0000</td>
+            <td className='income'>R$ 12.0000</td>
             <td>Free Lancer</td>
             <td>28/2/2022</td>
           </tr>
           <tr>
-            <td>Desenvolvimento de website</td>
-            <td>R$ 12.0000</td>
+            <td>Aluguel</td>
+            <td className='outcome'>-R$ 1.000</td>
             <td>Free Lancer</td>
-            <td>28/2/2022</td>
+            <td>2/3/2022</td>
           </tr>
           <tr>
-            <td>Desenvolvimento de website</td>
-            <td>R$ 12.0000</td>
-            <td>Free Lancer</td>
-            <td>28/2/2022</td>
+            <td>Salário</td>
+            <td className='income'>R$ 30.0000</td>
+            <td>BairesDev</td>
+            <td>5/3/2022</td>
           </tr>
         </tbody>
       </table>
--- src/components/TransactionsTabble/styles.ts
+++ src/components/TransactionsTabble/styles.ts
@@ -5,6 +5,34 @@ export const Container = styled.div`
 
   table {
     width: 100%;
-    max-width: 1120px;
+    border-spacing: 0 0.5rem;
+
+    th {
+      color: var(--text-body);
+      font-weight: 400;
+      padding: 1rem 2rem;
+      text-align: left;
+      line-height: 1.5rem;
+    }
+
+    td {
+      padding: 1rem 2rem;
+      border: 0;
+      background: var(--shape);
+      color: var(--text-body);
+      border-radius: 0.25rem;
+
+      &:first-child {
+        color: var(--text-title);
+      }
+
+      &.income {
+        color: var(--green);
+      }
+
+      &.outcome {
+        color: var(--red);
+      }
+    }
   }
 `;
